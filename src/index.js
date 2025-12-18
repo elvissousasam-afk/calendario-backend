f8e7de69a7a67ed0cb165121980b823f0dddd808
elvissousasam-afk <elvissousasam@gmail.com>
2025-12-18 14:20:25 -0300

    corrigir caminho do import de eventos

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import eventosRoutes from './eventos.js';
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 app.use('/eventos', eventosRoutes);
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`API rodando na porta ${PORT}`);
