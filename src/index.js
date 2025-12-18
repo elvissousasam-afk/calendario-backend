0f81c3b3f32fae322962015d4ec32d5286ef8273
elvissousasam-afk <elvissousasam@gmail.com>
2025-12-18 14:51:18 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import eventosRoutes from './eventos.js';
+import eventosRoutes from './routes/eventos.js';
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 app.use('/eventos', eventosRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`API rodando na porta ${PORT}`);
