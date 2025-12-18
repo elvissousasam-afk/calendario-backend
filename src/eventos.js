03f27216ee768509e5e619ffc98f02c3afac9104
elvissousasam-afk <elvissousasam@gmail.com>
2025-12-18 16:45:14 -0300

    Update eventos.js

--- src/eventos.js
+++ src/eventos.js
@@ -1,1 +1,9 @@
+// Funções relacionadas a eventos
+
+export function listarEventos() {
+  return [
+    { id: 1, nome: "Evento 1", data: "2025-12-20" },
+    { id: 2, nome: "Evento 2", data: "2025-12-25" }
+  ];
+}
 
